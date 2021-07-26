1a03acef387c25609f27c6e692ea43837d4ba044
enmanuel <enmanuellassis@gmail.com>
2021-07-26 13:14:23 -0400

    correct listItem error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 import _ from 'lodash';
 import './style.css';
-import { listItem } from './crud.js';
+import { ListItem } from './crud.js';
 import { interactions } from './interactions.js';
 import { dragDrop } from './dragDrop';
 
 
 const component = () => {
  
-  listItem.init();
+  ListItem.init();
   interactions.init();
   dragDrop();
 
